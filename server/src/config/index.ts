296e8bc7634d735435022a58fe19726eafc83263
Vladyslav Zhytnyk <Vlad@Vlad-MacBook-Pro.local>
2023-02-16 14:47:38 +0200

    added git

---
+++ server/src/config/index.ts
@@ -0,0 +1,19 @@
+import { config } from "dotenv";
+
+config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+
+export const CREDENTIALS = process.env.CREDENTIALS === 'true';
+export const {
+  NODE_ENV,
+  PORT,
+  MONGO_CONNECT_LINK,
+  AUTH0_AUDIENCE,
+  AUTH0_ISSUER,
+  LOG_FORMAT,
+  LOG_DIR,
+  ORIGIN,
+  AUTH0_MANAGEMENT_SERVICE_CLIENT_ID,
+  AUTH0_MANAGEMENT_SERVICE_CLIENT_SECRET,
+  CLIENT_URL,
+  AUTH0_MANAGEMENT_SERVICE_DOMAIN
+} = process.env;
